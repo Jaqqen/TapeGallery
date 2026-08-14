e86e6a55f211d52b68c75c59659023c36c541698
Jaqqen <hussain.daniyal@outlook.com>
2026-08-14 15:21:30 -0400

    feat(tapes-hub): set distributionsUrl zip-version to 9.6.1

--- tapes-hub/src/main/java/com/jaqqen/tapeshub/TapesHubApplication.java
+++ tapes-hub/src/main/java/com/jaqqen/tapeshub/TapesHubApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TapesHubApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(TapesHubApplication.class, args);
     }
 
