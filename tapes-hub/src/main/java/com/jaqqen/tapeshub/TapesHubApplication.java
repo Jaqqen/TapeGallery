0d82c02c2264a5f794944055224a1be884458db7
Jaqqen <hussain.daniyal@outlook.com>
2026-08-13 23:06:03 -0400

    feature(tapes-hub)!: new system to receive requests from web-portal

---
+++ tapes-hub/src/main/java/com/jaqqen/tapeshub/TapesHubApplication.java
@@ -0,0 +1,13 @@
+package com.jaqqen.tapeshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapesHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapesHubApplication.class, args);
+    }
+
+}
---
+++ tapes-hub/src/test/java/com/jaqqen/tapeshub/TapesHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaqqen.tapeshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapesHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
